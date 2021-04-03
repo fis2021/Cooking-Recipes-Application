90f1b2ffa7f032273ef4f6459163a1e19fd09819
Edward Roșcu <edward.roscu@yahoo.com>
2021-04-03 21:35:48 +0300

    Login try

--- src/main/java/org/tnh/App.java
+++ src/main/java/org/tnh/App.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 /**
  * JavaFX App
  */
---
+++ src/main/java/org/tnh/Main.java
@@ -0,0 +1,37 @@
+package org.tnh;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.tnh.services.FileSystemService;
+import org.tnh.services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        initDirectory();
+        UserService.initDatabase();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("register.fxml")));
+        primaryStage.setTitle("Registration Example");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    private void initDirectory() {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
