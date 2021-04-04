9c90d1131c07959f01c7e992f3f4435cfdf01147
Edward Roșcu <edward.roscu@yahoo.com>
2021-04-04 20:53:31 +0300

    Styling test

--- src/main/java/org/tnh/App.java
+++ src/main/java/org/tnh/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        scene.getStylesheets().add("styling.css");
         stage.setScene(scene);
         stage.show();
     }
