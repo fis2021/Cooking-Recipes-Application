704506d0b744fa42ff333f833b83be7b899a5285
PavaloniRobert <robertpavaloni662000@yahoo.com>
2021-05-10 00:48:45 +0300

    Added new fields for registration

--- src/main/java/org/tnh/controllers/RegistrationController.java
+++ src/main/java/org/tnh/controllers/RegistrationController.java
@@ -1,21 +1,40 @@
 package org.tnh.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.tnh.exceptions.UsernameAlreadyExistsException;
 import org.tnh.services.UserService;
 
+import java.util.Objects;
+
 public class RegistrationController {
+
+    private Stage stage;
+    private Parent root;
 
     @FXML
     private Text registrationMessage;
     @FXML
-    private PasswordField passwordField;
+    private PasswordField password;
     @FXML
-    private TextField usernameField;
+    private PasswordField confirmPassword;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField email;
     @FXML
     private ChoiceBox<String> role;
 
@@ -27,10 +46,18 @@ public class RegistrationController {
     @FXML
     public void handleRegisterAction() {
         try {
-            UserService.addUser(usernameField.getText(), passwordField.getText(), role.getValue());
+            UserService.addUser(firstName.getText(), lastName.getText(), email.getText(), username.getText(), password.getText(), confirmPassword.getText(), role.getValue());
             registrationMessage.setText("Account created successfully!");
         } catch (UsernameAlreadyExistsException e) {
             registrationMessage.setText(e.getMessage());
         }
+    }
+
+    public void handleBackFirstPage(ActionEvent event) throws Exception {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Cooking-Recipes-Application");
+        stage.setScene(new Scene(root, 900, 550));
+        stage.show();
     }
 }
