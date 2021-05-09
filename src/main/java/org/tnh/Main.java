81323d470bd667235544c6c86e52ab81d650e2fd
PavaloniRobert <robertpavaloni662000@yahoo.com>
2021-05-10 00:25:05 +0300

    Login GUI modification

--- src/main/java/org/tnh/Main.java
+++ src/main/java/org/tnh/Main.java
@@ -5,16 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.tnh.services.FileSystemService;
+import org.tnh.services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        initDirectory();
+        UserService.initDatabase();
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
         primaryStage.setTitle("Cooking-Recipes-Application");
         primaryStage.setScene(new Scene(root, 900, 550));
         primaryStage.show();
+    }
+
+    private void initDirectory() {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath)) {
+            boolean wasSuccessful;
+            wasSuccessful = applicationHomePath.toFile().mkdirs();
+            if (!wasSuccessful) {
+                System.out.println("was not successful.");
+            }
+        }
     }
 
     public static void main(String[] args)
