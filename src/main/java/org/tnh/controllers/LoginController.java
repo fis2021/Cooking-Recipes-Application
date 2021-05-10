fc8baa3092537cbb8eb5ab94efeda4ab18682146
PavaloniRobert <robertpavaloni662000@yahoo.com>
2021-05-10 22:56:27 +0300

    Modifications

--- src/main/java/org/tnh/controllers/LoginController.java
+++ src/main/java/org/tnh/controllers/LoginController.java
@@ -57,4 +57,12 @@ public class LoginController
             loginMessage.setText(e.getMessage());
         }
     }
+//de modificat
+    public void handleShowRecipes(ActionEvent event) throws Exception {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("recipe.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Recipes");
+        stage.setScene(new Scene(root, 900, 550));
+        stage.show();
+    }
 }
