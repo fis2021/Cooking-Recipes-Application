8ea2277637a21eb73b74cf5ab19780f1be535371
Edward Roșcu <edward.roscu@yahoo.com>
2021-05-10 18:38:54 +0300

    Created recipe class and corresponding service

---
+++ src/main/java/org/tnh/model/Recipe.java
@@ -0,0 +1,34 @@
+package org.tnh.model;
+
+import java.util.ArrayList;
+
+public class Recipe {
+
+    private String name;
+    private String instructions;
+    private String calories;
+    private String time;
+
+
+    public Recipe(String name, String calories, String time, String instructions) {
+        this.name = name;
+        this.calories = calories;
+        this.time = time;
+        this.instructions = instructions;
+    }
+
+    public String toString() {
+
+        return name + "\n\nNumber of calories: " + calories + "\n\nTime: " + time + " minutes\n\n" + instructions;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
