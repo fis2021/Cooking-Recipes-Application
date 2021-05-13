0137d93f15e3e7ec6a727bcd16a520f9019b3c38
PavaloniRobert <robertpavaloni662000@yahoo.com>
2021-05-13 15:48:48 +0300

    Head Chef own recipes button

--- src/main/java/org/tnh/model/Recipe.java
+++ src/main/java/org/tnh/model/Recipe.java
@@ -2,12 +2,14 @@ package org.tnh.model;
 
 public class Recipe {
 
+    private String username;
     private String name;
     private String calories;
     private String time;
     private String instructions;
 
-    public Recipe(String name, String calories, String time, String instructions) {
+    public Recipe(String username, String name, String calories, String time, String instructions) {
+        this.username = username;
         this.name = name;
         this.calories = calories;
         this.time = time;
@@ -18,6 +20,14 @@ public class Recipe {
 
     public String toString() {
         return name + "\n\nNumber of calories: " + calories + "\n\nTime: " + time + " minutes\n\n" + instructions;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getName() {
