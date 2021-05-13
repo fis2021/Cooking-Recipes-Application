4e0e90a7c67440b5db6f2133e3af4650e88e04d1
Edward Roșcu <edward.roscu@yahoo.com>
2021-05-13 11:49:44 +0300

    removed duplicate code

---
+++ src/main/java/org/tnh/controllers/AbstractListRecipesController.java
@@ -0,0 +1,36 @@
+package org.tnh.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public abstract class AbstractListRecipesController {
+
+    public void handleBackAction(ActionEvent event) throws Exception {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
+        if(stage.getTitle().equals("Head Chef - List of recipes"))
+        {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("head.fxml")));
+            stage.setTitle("Head Chef");
+        }
+        if(stage.getTitle().equals("Junior Chef - List of recipes"))
+        {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("junior.fxml")));
+            stage.setTitle("Junior Chef");
+        }
+        if(stage.getTitle().equals("List of recipes"))
+        {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
+            stage.setTitle("Cooking-Recipes-Application");
+        }
+        stage.setScene(new Scene(root, 1280, 720));
+        stage.show();
+    }
+
+}
--- src/main/java/org/tnh/controllers/SearchController.java
+++ src/main/java/org/tnh/controllers/SearchController.java
@@ -2,21 +2,16 @@ package org.tnh.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
 import org.tnh.model.Recipe;
 import org.tnh.services.RecipeService;
+
 import java.util.Objects;
 
-public class SearchController {
+public class SearchController extends AbstractListRecipesController {
 
     @FXML
     private TableColumn<Recipe, String> recipeName, recipeCalories, recipeTime, recipeInstructions;
@@ -33,30 +28,8 @@ public class SearchController {
         recipeTime.setCellValueFactory(new PropertyValueFactory<>("Time"));
         recipeInstructions.setCellValueFactory(new PropertyValueFactory<>("Instructions"));
         ObservableList<Recipe> recipes_obs = FXCollections.observableArrayList();
-        recipes_obs.addAll(RecipeService.populateDataSearch(LoginController.getSearch()));
+        recipes_obs.addAll(Objects.requireNonNull(RecipeService.populateDataSearch(LoginController.getSearch())));
         recipesTableView.setItems(recipes_obs);
-    }
-
-    public void handleBackAction(ActionEvent event) throws Exception {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
-        if(stage.getTitle().equals("Head Chef - List of recipes"))
-        {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("head.fxml")));
-            stage.setTitle("Head Chef");
-        }
-        if(stage.getTitle().equals("Junior Chef - List of recipes"))
-        {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("junior.fxml")));
-            stage.setTitle("Junior Chef");
-        }
-        if(stage.getTitle().equals("List of recipes"))
-        {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
-            stage.setTitle("Cooking-Recipes-Application");
-        }
-        stage.setScene(new Scene(root, 1280, 720));
-        stage.show();
     }
 
 }
