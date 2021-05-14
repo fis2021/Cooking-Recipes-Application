4f05311e4651931f9245ff3aaed8d377d1b87910
Edward Roșcu <edward.roscu@yahoo.com>
2021-05-14 13:30:15 +0300

    Fixed exception cases

---
+++ src/main/java/org/tnh/exceptions/CouldNotFindRecipeException.java
@@ -0,0 +1,10 @@
+package org.tnh.exceptions;
+
+public class CouldNotFindRecipeException extends Exception {
+
+    public CouldNotFindRecipeException() {
+        super("Could not find recipes");
+    }
+
+}
+
