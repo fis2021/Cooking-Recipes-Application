74c526235d25a7b5e2dd973aef647df4265bbd69
Edward Roșcu <edward.roscu@yahoo.com>
2021-05-16 01:08:23 +0300

    added delete account functionality

--- src/main/java/org/tnh/controllers/AbstractChefController.java
+++ src/main/java/org/tnh/controllers/AbstractChefController.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import org.tnh.model.LoggedUser;
+import org.tnh.services.UserService;
 
 import java.util.Objects;
 
@@ -22,6 +24,21 @@ public abstract class AbstractChefController {
         alert.setHeaderText("Are you sure you want to log out?");
         alert.showAndWait();
         if(alert.getResult().getText().equals("Yes")) {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("startup_page.fxml")));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setTitle("Cooking-Recipes-Application");
+            stage.setScene(new Scene(root, 1280, 720));
+            stage.show();
+        }
+    }
+
+    public void handleDeleteAccountAction(ActionEvent event) throws Exception {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "", ButtonType.YES, ButtonType.NO);
+        alert.getDialogPane().lookupButton(ButtonType.YES).setId("Yes");
+        alert.setHeaderText("Are you sure you want to delete your account?");
+        alert.showAndWait();
+        if(alert.getResult().getText().equals("Yes")) {
+            UserService.deleteAccount(LoggedUser.getLoggedUser().getUsername());
             root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("startup_page.fxml")));
             stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setTitle("Cooking-Recipes-Application");
