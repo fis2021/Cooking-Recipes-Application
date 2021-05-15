9e64386609ec242374c60fb68a2bf2420c839c80
Edward Roșcu <edward.roscu@yahoo.com>
2021-05-16 00:31:43 +0300

    tested save recipe window

---
+++ src/test/java/org/tnh/SaveRecipeWindowTest.java
@@ -0,0 +1,109 @@
+package org.tnh;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.base.WindowMatchers;
+import org.tnh.exceptions.*;
+import org.tnh.model.LoggedUser;
+import org.tnh.model.Recipe;
+import org.tnh.model.User;
+import org.tnh.services.FileSystemService;
+import org.tnh.services.RecipeService;
+import org.tnh.services.UserService;
+
+import java.util.Objects;
+
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+@ExtendWith(ApplicationExtension.class)
+class SaveRecipeWindowTest {
+
+    private final String FIRST_NAME = "Edward";
+    private final String LAST_NAME = "Rosco";
+    private final String EMAIL = "edward@yahoo.com";
+    private final String USERNAME = "Edward26";
+    private final String PASSWORD = "Edw@rd62";
+    private final String ROLE = "Junior Chef";
+
+    private final String RECIPE_NAME = "Chicken";
+    private final String CALORIES = "300";
+    private final String TIME = "55";
+    private final String INSTRUCTIONS = "https://www.ambitiouskitchen.com/the-best-chicken-soup-recipe/";
+
+    @BeforeAll
+    static void beforeAll() {
+        FileSystemService.APPLICATION_FOLDER = ".test-cooking-recipes";
+        FileSystemService.initDirectory();
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
+        UserService.initDatabase();
+        RecipeService.initDatabase();
+        LoggedUser.setLoggedUser(new User(FIRST_NAME, LAST_NAME, EMAIL, USERNAME, PASSWORD, ROLE));
+    }
+
+    @AfterEach
+    void tearDown() {
+        UserService.closeDatabase();
+        RecipeService.closeDatabase();
+    }
+
+    @SuppressWarnings("unused")
+    @Start
+    void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("save_recipe.fxml")));
+        primaryStage.setTitle("Junior Chef - Save");
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    @Test
+    void testAllFieldsAreCompletedAndCorrect(FxRobot robot) throws RecipeAlreadyExistsException, UncompletedFieldsException {
+        robot.clickOn("#enter_button");
+        assertThat(robot.lookup("#searchMessage").queryText()).hasText("Complete all fields!");
+
+        robot.clickOn("#search");
+        robot.write(RECIPE_NAME);
+
+        robot.clickOn("#enter_button");
+        assertThat(robot.lookup("#searchMessage").queryText()).hasText("Could not find recipes!");
+
+        RecipeService.addRecipe(USERNAME + 1, RECIPE_NAME, CALORIES, TIME, INSTRUCTIONS);
+
+        robot.clickOn("#enter_button");
+        assertThat(robot.lookup("#searchMessage").queryText()).hasText("The recipe has been saved to favorites!");
+    }
+
+    @Test
+    void testRecipeIsSavedCorrectly(FxRobot robot) throws RecipeAlreadyExistsException, UncompletedFieldsException {
+        RecipeService.addRecipe(USERNAME + 1, RECIPE_NAME, CALORIES, TIME, INSTRUCTIONS);
+        robot.clickOn("#search");
+        robot.write(RECIPE_NAME);
+        robot.clickOn("#enter_button");
+        Recipe recipe = RecipeService.populateSavedRecipesList(USERNAME).get(0);
+        Assertions.assertThat(recipe.getCalories()).isEqualTo(CALORIES);
+    }
+
+    @Test
+    void testCanGoToPreviousPage(FxRobot robot) {
+        robot.clickOn("#back_button");
+        FxAssert.verifyThat(robot.window("Junior Chef"), WindowMatchers.isShowing());
+    }
+
+}
